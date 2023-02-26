4b4efd20041b2a16659ed2295338681aed0e1cec
Daniil <hokkeict_@mail.ru>
2023-02-26 17:39:53 +0300

    Решение задачи № 2

--- Lessons_1/Home_work/Task_4/Program.cs
+++ Lessons_1/Home_work/Task_4/Program.cs
@@ -4,5 +4,18 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
-Console.WriteLine("Введите число 1: ");
+Console.Write("Введите число 1: ");
+string a = Console.ReadLine()!;
+int num1 = int.Parse(a);
+Console.Write("Введите число 2: ");
+string b = Console.ReadLine()!;
+int num2 = int.Parse(b);
+Console.Write("Введите число 3: ");
+string c = Console.ReadLine()!;
+int num3 = int.Parse(c);
+int max_num = num1;
 
+if (max_num < num2) max_num = num2;
+if (num3 > max_num) max_num = num3;
+Console.Write("Ответ: ");
+Console.WriteLine(max_num);
