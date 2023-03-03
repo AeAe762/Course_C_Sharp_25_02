ac70ff4d8e4507a10cda9427e8af4cbf51356143
Daniil <hokkeict_@mail.ru>
2023-03-03 23:15:13 +0300

    Решение 2 задачи с семинара

---
+++ Lessons_2/Home_work/Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное число и 
+// удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int TakeNum(int num)
+{
+  Console.WriteLine("Введите число: ");
+  Console.WriteLine(num);
+  int num1 = num % 10; // Последняя цифра
+  int num2 = num % 1000 / 100;// первая цифра
+  int result = num2 * 10 + num1;
+  return result;
+}
+//int otvet = TakeNum(int.Parse(Console.ReadLine()!));
+int otvet = TakeNum(new Random().Next(100, 1000));
+Console.WriteLine(otvet);
