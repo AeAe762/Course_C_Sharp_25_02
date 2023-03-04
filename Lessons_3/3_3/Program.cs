15cd1f66d06cd628dea2329681f1f057f63d6597
Daniil <hokkeict_@mail.ru>
2023-03-04 18:13:25 +0300

    Решение задач семинар № 3

---
+++ Lessons_3/3_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// // 3. Напишите программу, которая принимает на вход число (N) 
+//    и выдаёт таблицу квадратов чисел от 1 до N.
+
+void Kvad (int num)
+{
+  int num_1 = 1;
+  while (num_1 <= num)
+  {
+    int result = num * num;
+    num_1 = num_1 + 1;
+    return;
+  }
+}
+  int num = int.Parse(Console.WriteLine()!);
+  Kvad(num);
