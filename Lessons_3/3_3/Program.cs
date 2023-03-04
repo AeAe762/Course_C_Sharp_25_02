ce40a2a1bc9d1b75798a0151b7715545e537dc8d
Daniil <hokkeict_@mail.ru>
2023-03-04 19:29:04 +0300

    Разобраться с 3 задачей

--- Lessons_3/3_3/Program.cs
+++ Lessons_3/3_3/Program.cs
@@ -1,15 +1,18 @@
 ﻿// // 3. Напишите программу, которая принимает на вход число (N) 
 //    и выдаёт таблицу квадратов чисел от 1 до N.
 
-void Kvad (int num)
+void Kvad(int num)
 {
   int num_1 = 1;
-  while (num_1 <= num)
+  Console.Write(num);
+  while (num >= num_1)
   {
     int result = num * num;
-    num_1 = num_1 + 1;
-    return;
+    Console.Write(result);
+    num_1++;
   }
+  Console.WriteLine();
 }
-  int num = int.Parse(Console.WriteLine()!);
-  Kvad(num);
+int num = int.Parse(Console.ReadLine()!);
+Kvad(num);
+
