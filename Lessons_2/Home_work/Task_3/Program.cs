f0da364adbbc0a816e3a08c8d0b990584c7b9b68
Daniil <hokkeict_@mail.ru>
2023-03-04 09:39:47 +0300

    Доделать задачу 3

--- Lessons_2/Home_work/Task_3/Program.cs
+++ Lessons_2/Home_work/Task_3/Program.cs
@@ -4,3 +4,15 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+int num = int.Parse(Console.ReadLine()!);
+if (num < 100) Console.WriteLine("Третьей цифры нет");
+else 
+{
+  while (num > 999); 
+  num / 10;
+}
+int last_num = num % 10;
+Console.WriteLine(las_num);
+
+  
+ 
