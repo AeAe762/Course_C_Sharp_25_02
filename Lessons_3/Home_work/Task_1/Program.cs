e2ab9a1fd2145e77a7faefc57cc6a790c39dbe1d
Daniil <hokkeict_@mail.ru>
2023-03-08 01:41:00 +0300

    Решение задачи № 1

--- Lessons_3/Home_work/Task_1/Program.cs
+++ Lessons_3/Home_work/Task_1/Program.cs
@@ -5,4 +5,25 @@
 // 12821 -> да
 // 23432 -> да
 
+Console.WriteLine("Вариант 1");
+Console.Write("Введите число: ");
+void Task(int num)
+{
+  if (num / 10000 == num % 10 && num / 1000 % 10 == num % 100 / 10) Console.WriteLine($"Число {num} является палиндромом");
+  else Console.WriteLine($"Число {num} не является палиндромом");
+  return;
+}
+Task(int.Parse(Console.ReadLine()!));
+
+
+
+Console.WriteLine("Вариант 2");
+Console.Write("Введите число: ");
+void Task_2(string num)
+{
+  if (num[0] == num[4] && num[1] == num[3]) Console.WriteLine($"Число {num} является палиндромом");
+  else Console.WriteLine($"Число {num} не является палиндромом");
+  return;
+}
+Task_2(Console.ReadLine()!);
 
