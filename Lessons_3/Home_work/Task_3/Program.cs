3f7b52931ef2ff76fc4acd31adaaa1a27d88f29b
Daniil <hokkeict_@mail.ru>
2023-03-08 02:40:07 +0300

    Решение задачи № 3

---
+++ Lessons_3/Home_work/Task_3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов 
+// чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void Kub(int num){int num_1 = 1; while (num_1 <= num) {Console.Write($"{Math.Pow(num_1, 3)} ");
+num_1++; } Console.WriteLine();}
+Kub(int.Parse(Console.ReadLine()!));
