e29ed2b8ec3d7fc1e9f0682f163e0c2f157ce4e5
Daniil <hokkeict_@mail.ru>
2023-03-10 22:28:28 +0300

    Решение задачи № 1 и № 2

---
+++ Lessons_4/4_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход число и выдаёт количество 
+// цифр в числе.
+// 456 -> 3 
+// 78 -> 2 
+// 89126 -> 5
+
+int Count_num (int num)
+{
+  int count = 0;
+  for (int i = 0; num > 0; i++)
+    {
+       num = num / 10;
+       count ++;
+    }
+  return count;   
+}
+int result = Count_num(int.Parse(Console.ReadLine()!));
+Console.WriteLine($"Количество цифр в числе = {result}");
