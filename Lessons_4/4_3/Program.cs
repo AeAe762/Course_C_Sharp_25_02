359a3adccdd6fb05ef64296e29d20b23a9597ff4
Daniil <hokkeict_@mail.ru>
2023-03-10 22:53:34 +0300

    Решение задачи № 3

---
+++ Lessons_4/4_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 3: Напишите программу, которая выводит массив из 8 элементов, заполненный 
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+void Massive (int lenght)
+{
+  int[] R = new int [lenght];
+  for (int i = 0; i < lenght; i++)
+  {
+    R[i] = new Random().Next(2);
+    Console.Write($"{R[i]} ");
+  }
+}
+Massive(int.Parse(Console.ReadLine()!));
