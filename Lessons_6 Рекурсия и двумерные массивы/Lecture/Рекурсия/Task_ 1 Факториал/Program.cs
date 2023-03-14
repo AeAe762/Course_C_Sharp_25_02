a903b2308ae7e22863d72314223365c0f0e2815c
Daniil <hokkeict_@mail.ru>
2023-03-14 22:57:42 +0300

    Рекурсия и двумеирные массивы

---
+++ Lessons_6 Рекурсия и двумерные массивы/Lecture/Рекурсия/Task_ 1 Факториал/Program.cs
@@ -0,0 +1,9 @@
+﻿double Factorial (int num)
+{
+  if (num == 1) return 1;
+  else return num * Factorial(num - 1);
+}
+for (int i = 1; i < 40; i++)
+{
+  Console.WriteLine($"{i}! = {Factorial(i)}");
+}
