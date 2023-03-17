eebc403933710d6ba2a625a09b1d20258ed41908
Daniil <hokkeict_@mail.ru>
2023-03-18 00:57:18 +0300

    Решение задачи № 34

---
+++ Lessons_4 Методы/1_Lecture/Task_4 Упорядочить массив/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void Print_Array(int[] array) //Будет выводить массив
+{
+  int length = array.Length; // Считает размер массива
+  for (int i = 0; i < length; i++)
+  {
+    Console.Write($"{array[i]} ");
+  }
+  Console.WriteLine();
+}
+void Selection_Sort(int [] array) //упорядочивает массив
+{
+  for (int i = 0; i < array.Length - 1; i++)
+  {
+    int min_position = i; // присваивает переменной значение
+    for (int j = i; j < array.Length; j++)
+    {
+      if (array[j] < array[min_position]) min_position = j;
+    }
+    int temporary = array[i];
+    array[i] = array[min_position]; // меняем значение массива местами;
+    array[min_position] = temporary; // меняем значение массива местами;
+  }
+}
+Print_Array(arr);
+Selection_Sort(arr);
+Print_Array(arr);
