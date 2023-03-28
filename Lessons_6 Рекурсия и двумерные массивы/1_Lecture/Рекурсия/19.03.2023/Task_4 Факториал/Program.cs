6787d323a23098c38f0d0d89a188ec2874db3310
Daniil <hokkeict_@mail.ru>
2023-03-29 00:12:49 +0300

    Задача 4: Задайте две матрицы. Напишите программу, которая будет находить // произведение двух матриц. // Например, даны 2 матрицы: // 2 4 | 3 4 // 3 2 | 3 3 // Результирующая матрица будет: // 18 20 // 15 18

---
+++ Lessons_6 Рекурсия и двумерные массивы/1_Lecture/Рекурсия/19.03.2023/Task_4 Факториал/Program.cs
@@ -0,0 +1,16 @@
+﻿// Факториал числа
+
+
+ int FactorialFor(int n) // Цикл
+ {
+     int result = 1;
+     for (int i = 1; i <= n; i++) result *= i;
+     return result;
+ }
+ int FactorialRec(int n) // Рекурсия
+ {
+     if (n == 1) return 1;
+     else return n * FactorialRec(n - 1);
+ }
+ Console.WriteLine(FactorialFor(10)); // 3628800
+ Console.WriteLine(FactorialRec(10)); // 3628800
