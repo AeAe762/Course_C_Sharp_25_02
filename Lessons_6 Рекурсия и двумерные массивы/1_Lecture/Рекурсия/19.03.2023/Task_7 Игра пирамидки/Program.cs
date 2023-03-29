cb475b9fe685c17e688fc0eecece5264f71802e2
Daniil <hokkeict_@mail.ru>
2023-03-29 23:43:06 +0300

    Решение задач из лекции от 19.03.2023

---
+++ Lessons_6 Рекурсия и двумерные массивы/1_Lecture/Рекурсия/19.03.2023/Task_7 Игра пирамидки/Program.cs
@@ -0,0 +1,13 @@
+﻿// http://rebus1.com/index.php?item=tower
+
+ void Towers(string with = "1", string on = "3", string some = "2", int count = 3)
+ {
+    if (count > 1) Towers(with, some, on, count - 1);
+    Console.WriteLine($"{with} >> {on}");
+    if (count > 1) Towers(some, on, with, count - 1);
+}
+
+// string with = "1" переменная с которой будут перекладываться кружки
+// string on = "3" переменная на которую будут перекладываться кружки
+// string some = "2" промежуточная переменная
+// int count = 3 количество кружков
